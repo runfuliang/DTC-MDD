101b697a261b370a3e15e3e95ff66ee099ac64f6
runfuliang <138708028+runfuliang@users.noreply.github.com>
2023-07-06 23:21:19 +0800

    NIEC.py

--- NIEC.py
+++ NIEC.py
@@ -1,50 +1,60 @@
-function H_a(m, K):
+from sympy import isprime, randprime
+from random import randint
+import hashlib
+import numpy as np
+
+def H_a(m, K):
     """Hash function"""
-    return int(hashlib.sha256(str(m | K).encode()).hexdigest(), 16)
+    return int(hashlib.sha256((str(m) + str(K)).encode()).hexdigest(), 16)
 
-function generate_large_prime(n):
-    """This function generates a large prime number of n bits"""
-    return randprime(2^(n-1), 2^n)
+def H_j(j, e, N):
+    return int(hashlib.sha256((str(j) + str(e)).encode()).hexdigest(), 16) % N
 
-function encryption(m, r, g, h, n, K):
-    """Encryption function"""
-    return (pow(g, H_a(m, K), n^2) * pow(h, r, n^2)) % n^2
+def generate_large_prime(n):
+    return randprime(2 ** (n-1), 2 ** n)
 
-function initialize_bloom_filter(m_l, m_u, g, n, K):
-    """Initialize bloom filter"""
-    bloom_filter = set()
-    for i in range(m_l, m_u + 1):
-        bloom_filter.add(pow(g, H_a(i, K), n^2))
-    return bloom_filter
+def encryption(m, r, g, h, n, K):
+    return (pow(g, H_a(m, K), n**2) * pow(h, r, n**2)) % n**2
 
-function niec(m1, m2, kappa, m_l, m_u):
-    """NIEC protocol"""
-    p = generate_large_prime(kappa)
-    q = generate_large_prime(kappa)
-    n = p * q
-    g = random_integer(1, n^2)
-    h = pow(g, q, n^2)
-    K = random_integer(1, 2^kappa)
+def initialize_bloom_filter(m_l, m_u, g, p, n, K, N, k):
+    A = np.zeros(N, dtype=int)
+    for m1 in range(m_l, m_u + 1):
+        for m2 in range(m_l, m_u + 1):
+            if m1 > m2:
+                e = pow(g, p * (H_a(m1, K) - H_a(m2, K)), n**2)
+                for j in range(1, k + 1):
+                    A[H_j(j, e, N)] = 1
+    return A
 
-    bloom_filter = initialize_bloom_filter(m_l, m_u, g, n, K)
-
-    r1 = random_integer(1, n-1)
-    r2 = random_integer(1, n-1)
-    
-    c1 = encryption(m1, r1, g, h, n, K)
-    c2 = encryption(m2, r2, g, h, n, K)
-    
-    # Preprocessing
-    c = (c1 * pow(c2, n^2 - 2, n^2)) % n^2
-    
-    # Comparison
-    C = pow(c, p, n^2)
-    
+def compare(c1, c2, p, n, BF, N, k):
+    c = (c1 * pow(c2, -1, n**2)) % n**2
+    C = pow(c, p, n**2) % n**2
     if C == 1:
         return "m1 = m2"
-    elif C in bloom_filter:
-        return "m1 > m2"
     else:
-        return "m1 < m2"
+        for j in range(1, k + 1):
+            if BF[H_j(j, C, N)] == 0:
+                return "m1 < m2"
+        return "m1 > m2"
 
+kappa = 256
+m_l = 1
+m_u = 100
+N = 100
+k = 5
 
+p = generate_large_prime(kappa)
+q = generate_large_prime(kappa)
+n = p * q
+g = n + 1
+h = pow(g, q, n**2) 
+K = randint(1, 2**kappa)
+
+BF = initialize_bloom_filter(m_l, m_u, g, p, n, K, N, k)
+
+m1 = 10
+m2 = 11
+r1 = randint(1, n-1)
+r2 = randint(1, n-1)
+c1 = encryption(m1, r1, g, h, n, K)
+c2 = encryption(m2, r2, g, h, n, K)
